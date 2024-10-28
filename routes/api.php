c1a893dafdd6aefb2f82a6de6f63925169c4aa18
Tiago Gomes <tiagosilvagomes85@gmail.com>
2024-10-28 12:59:29 +0000

    Protect API endpoints with authentication middleware (sanctum)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function (Request $request) {
+Route::get('public/test', function (Request $request) {
     return response()->json(['data'=> 'ok'], 200);
+});
+
+// Routes that require authentication
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('protected/test', function (Request $request) {
+        return response()->json(['data'=> 'ok'], 200);
+    });
 });
